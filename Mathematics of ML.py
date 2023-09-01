7320e2121d07ed0b3e53cb9fe5c42dc6f3bd173a
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-09-01 07:57:49 +0530

    Mean, Median, Mode, Variance, Standard Deviation

---
+++ Mathematics of ML.py
@@ -0,0 +1,25 @@
+import numpy as np
+import statistics as std
+
+height=[150,120,150,112,118]
+
+mean=np.mean(height)
+mean
+//130.0
+
+height=sort()
+median=np.median(height)
+median
+//120.0
+
+mode=stats.mode(height)
+mode
+//150
+
+variance=np.var(height)
+variance
+//273.6
+
+standardDeviation=np.std(height)
+standardDeviation
+//16.540858502508268
